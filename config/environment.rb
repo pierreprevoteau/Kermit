97054698e83de6f7643f355253e4991e44d333c1
Pierre Prevoteau <pierre.prevoteau@askida.fr>
2015-05-03 19:49:57 +0200

    work on docker

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Resque.configure do |config|
+
+  # Set the redis connection. Takes any of:
+  #   String - a redis url string (e.g., 'redis://host:port')
+  #   String - 'hostname:port[:db][/namespace]'
+  #   Redis - a redis connection that will be namespaced :resque
+  #   Redis::Namespace - a namespaced redis connection that will be used as-is
+  #   Redis::Distributed - a distributed redis connection that will be used as-is
+  #   Hash - a redis connection hash (e.g. {:host => 'localhost', :port => 6379, :db => 0})
+  config.redis = 'redis://192.168.99.100:32768'
+
+end
